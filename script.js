3f124bd37ac49dd078ac041e75fedddf33ed6715
shambhu621996 <shambhu621996@gmail.com>
2025-04-30 02:39:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ get width(){
 get height(){
 	return this._height
 }
-get area(){
+getArea(){
 	return this._width * this._height
 }
 }
@@ -18,20 +18,20 @@ class Square extends Rectangle {
 	constructor(side){
 		super(side,side);
 	}
-	get perimeter(){
+	getPerimeter(){
 		return 4*this.width
 	}
 }
 const rect = new Rectangle(5,10);
 console.log(rect.height);
 console.log(rect.width);
-console.log(rect.area);
+console.log(rect.getArea());
 
 const sq = new Square(7);
 console.log(sq.width);
 console.log(sq.height);
-console.log(sq.area);
-console.log(sq.perimeter);
+console.log(sq.getArea());
+console.log(sq.getPerimeter());
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
